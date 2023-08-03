05b328a6f3e469c19fe1a24d3a072be9f7ccccd1
Mathias <email@example.com>
2023-08-03 08:52:10 +0200

    init

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    println!("Hello, TTF_RUST!");
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
